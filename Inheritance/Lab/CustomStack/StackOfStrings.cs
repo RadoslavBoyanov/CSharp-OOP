43497aa7516e1caabc29994bf607b69185084468
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2022-12-14 17:10:22 +0200

    Inheritance - lab tasks

---
+++ Inheritance/Lab/CustomStack/StackOfStrings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return Count == 0;
+        }
+
+        public Stack<string> AddRange(IEnumerable<string> collection)
+        {
+            foreach (var element in collection)
+            {
+                this.Push(element);   
+            }
+            return this;
+        }
+    }
+}
---
+++ Inheritance/Lab/Farm/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Farm
+{
+    public class Cat : Animal
+    {
+        public void Meow()
+        {
+            Console.WriteLine("meowing…");
+        }
+    }
+}
