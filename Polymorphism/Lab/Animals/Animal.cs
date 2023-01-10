1fced347f1ecba84693915cdd6b5527ed654c7e0
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2023-01-10 11:13:21 +0200

    Polymorphism - lab projects

---
+++ Polymorphism/Lab/Animals/Animal.cs
@@ -0,0 +1,22 @@
+﻿namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private string favouriteFood;
+
+        public Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            FavouriteFood = favouriteFood;
+        }
+
+        public string Name { get; set; }
+        public string FavouriteFood { get; set; }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {Name} and my fovourite food is {FavouriteFood}";
+        }
+    }
+}
