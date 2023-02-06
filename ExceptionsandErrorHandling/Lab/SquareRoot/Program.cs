3297dd10d9857c90ac670fa18d5b84c45cfeee97
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2023-02-06 17:24:01 +0200

    Exception Handling

---
+++ ExceptionsandErrorHandling/Lab/SquareRoot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number < 0)
+                {
+                    throw new ArgumentException();
+                }
+                double result = Math.Sqrt(number);
+                Console.WriteLine(result);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
